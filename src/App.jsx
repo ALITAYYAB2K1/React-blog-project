9db20413aff9452af490e2eca8909045afeb586c
ALI <01-134232-037@student.bahria.edu.pk>
2025-02-24 16:38:16 +0500

    install tailwind and started app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import{Outlet} from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL) 
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+      {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally( ()=> setLoading(false))
+  })
+  return !loading ?(
+    <div className='min-h-screen flex-wrap content-between bg-slate-400 '>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO : <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
 }
+
 
 export default App
