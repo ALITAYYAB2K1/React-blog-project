f08876c11920a2f990a65f37c8d9b17364f961aa
ALI <01-134232-037@student.bahria.edu.pk>
2025-02-26 17:25:57 +0500

    Footer and logoutBtn and logo completed

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -6,8 +6,15 @@ import { useHistory } from 'react-router-dom'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
+    const logoutHandler = () => {
+      authService.logout().then().catch((error) => {
+        dispatch(logout())
+      })
+      
+    }
   return (
-    <div>LogoutBtn</div>
+    <button onClick={logoutHandler} 
+    className="btn btn-danger inline-block px-6 py-2 duration-200 hover:bg-blue-200">Logout</button>
   )
 }
 
