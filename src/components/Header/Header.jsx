8ab340938cebf254dfd79a80e74bb55351e2da35
ALI <01-134232-037@student.bahria.edu.pk>
2025-02-26 18:05:21 +0500

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 function Header() {
   const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
-  const naItems = [
+  const navItems = [
     {
       name: 'Home',
       slug: '/',
@@ -43,6 +43,21 @@ function Header() {
             <Logo width='70px' />
             </Link>
           </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item)=>
+              item.active ? (
+              <li key={item.name}>
+                <button onClick={()=>navigate(item.slug)} className='inline-block px-4 py-2 duration-200 hover:bg-blue-300 rounded-full'
+                >{item.name}</button>
+              </li>
+              ):null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
         </nav>
       </Container>
     </header>
