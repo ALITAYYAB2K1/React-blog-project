4ee9e92e7f3ee65ee6e8dc7735e6bea99abf8c17
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-02-28 17:54:50 +0500

    login form completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import {useState} from 'react'
 import { Link,useNavigate } from 'react-router'
 import {login as authLogin} from '../store/authSlice'
 import {Button,Input,Logo} from './index'
@@ -38,6 +38,51 @@ function Login() {
             <h2 className='text-center text-2xl font-bold leading-tight'>
                 Sign in to your account
             </h2>
+            <p className='text-center mt-2 text-base text-black/60'>
+                Don&apos;t have an account?&nbsp;
+                <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>
+                    Sign Up
+                </Link>
+
+            </p>
+            {error && <p className='text-red-500 text-center mt-8'>{error}</p>}
+            <form onSubmit={handleSubmit(login)} className='mt-8'>
+                <div className='space-y-5'>
+                    <Input 
+                        label='Email'
+                        placeholder='Enter your email'
+                        type='email'
+                        name='email'
+                        {...register('email',{
+                            required:true,
+                            validate:{
+                                matchPattern: (value)=>
+                                    /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm.test(value) || 'Please enter a valid email'
+                            }
+                        })}
+                    />
+                    <Input
+                        label='Password'
+                        placeholder='Enter your password'
+                        type='password'
+                        name='password'
+                        {...register('password',{
+                            required:true,
+                            minLength:{
+                                value:6,
+                                message:'Password must be at least 6 characters'
+                            }
+                        })}
+                    />
+                    <Button
+                        type='submit'
+                        className='w-full'
+                    >
+                        Sign in
+                    </Button>
+                </div>
+
+            </form>
         </div>
 
     </div>
