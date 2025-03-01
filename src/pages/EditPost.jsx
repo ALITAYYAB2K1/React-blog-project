a3c1580c21ae298d6cbd3c53dbb5680b50f849f3
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-01 22:25:54 +0500

    pag

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { Container, PostForm } from "../components";
+
+function EditPost() {
+  const [post, setPost] = useState(null);
+  return <div>EditPost</div>;
+}
+
+export default EditPost;
