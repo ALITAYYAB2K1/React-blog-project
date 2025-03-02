d79b6ce7ebfb9a360236753c1c6415c3a3b8bc18
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-02 11:38:28 +0500

    routes in main.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -6,10 +6,27 @@ import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Button from "./Button";
 import Input from "./Input";
-import Signup from "./Signup"
+import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import Post from "./Post";
 import PostCard from "./PostCard";
 import PostList from "./PostList";
-export { Header, Footer , Container, Logo , LogoutBtn,RTE,Button,Input,Signup,Login,PostForm ,Post,PostCard,PostList}; 
+import AuthLayout from "./AuthLayout";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Button,
+  Input,
+  Signup,
+  Login,
+  PostForm,
+  Post,
+  PostCard,
+  PostList,
+  AuthLayout,
+};
