13446f0699631cf97e0cf836ec02362c5b5f378f
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-02 10:39:01 +0500

    editpost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,9 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Container, PostForm } from "../components";
+import { useParams, Navigate } from "react-router-dom";
+import appwriteService from "../appwrite/config";
 
 function EditPost() {
   const [post, setPost] = useState(null);
-  return <div>EditPost</div>;
+  const { slug } = useParams();
+  const navigate = Navigate();
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          navigate("/");
+        }
+      });
+    }
+  }, [slug, navigate]);
+
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  )
 }
 
 export default EditPost;
